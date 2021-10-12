7d81f77a5b2be398ac87df98a39fc9632ab8d222
Oksana Denisova <oksana_denisova@bk.ru>
2021-10-12 17:30:44 +0500

    import

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -10,16 +10,16 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        with open('phones.csv', 'r') as csvfile:
+            phone_reader = csv.reader(csvfile, delimiter=';')
+            next(phone_reader)
+            for line in phone_reader:
+                phone = Phone()
+                phone.name = line[1]
+                phone.image = line[2]
+                phone.price = line[3]
+                phone.release_date = line[4]
+                phone.lte_exists = line[5]
+                phone.slug = slugify(line[1])
+                phone.save()
 
-        for phone in phones:
-            Phone.objects.create(
-                name=phone.get('name'),
-                price=phone.get('price'),
-                image=phone.get('image'),
-                release_date=phone.get('release_date'),
-                lte_exists=phone.get('lte_exists'),
-                slug=slugify(phone.get('name'))
-                )
-            # Phone.objects.save()
